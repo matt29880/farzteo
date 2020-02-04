ae8b2335350d141e82092bdcc3ced9810b4e1099
Mathieu Ronvel <mathieu@ronvel.com>
2020-02-04 21:46:20 +0100

    Improve publish button (#6)

--- admin-frontend/src/app/views/dashboard/dashboard.component.ts
+++ admin-frontend/src/app/views/dashboard/dashboard.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 import { PublishService } from './publish.service';
-import { PublishType } from './publishType';
 
 @Component({
   templateUrl: 'dashboard.component.html'
@@ -12,11 +11,11 @@ export class DashboardComponent {
 
   constructor(public publishService: PublishService) { }
 
-  publishType = PublishType;
   published : boolean = false;
 
-  publish(publishType : PublishType) {
-    this.publishService.publish(publishType).subscribe(res => {
+  publish() {
+    this.published = false;
+    this.publishService.publish().subscribe(res => {
       console.log("Published !!!");
       this.published = true;
     });
